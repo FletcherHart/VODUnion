8c54316ae0959a72fa7912ce2a018456bea2318b
FletcherHart <fletcherhart@gmail.com>
2020-12-01 12:39:34 -0500

    Change AppServiceProvider to share auth status with Inertia
    AppServiceProvider now shares user login status with Inertia, enabling front in login/logout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Inertia::share('auth.user', function (){
+            return[
+                'loggedIn' => Auth::check(),
+            ];
+        });
     }
 }
