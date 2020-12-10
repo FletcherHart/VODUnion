f7251358263f3bfbdcd19d1d5e6d62e70f707e2b
FletcherHart <fletcherhart@gmail.com>
2020-12-10 11:36:01 -0500

    Add basic upload form for manual tests

--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -1,7 +1,22 @@
 <template>
     <header-layout>
         <div class="py-12">
-            Upload
+            <div class="flex justify-center">
+                <h2 class="m-auto font-bold">Upload</h2>
+            </div>
+            <div class="flex justify-center">
+                <form @submit.prevent="submit" class="grid grid-cols-1 w-1/3">
+                    <input class="border-solid border-2 border-black-600" id="title" name="title" placeholder="Video Title" >
+                    <input class="border-solid border-2 border-black-600" id="description" name="description" rows=5 placeholder="Description">
+                    <input type="file" id="video" name="video" ref="video" accept=".mp4">
+                    <span>
+                        <label for="listed">List video immediatly upon upload?</label>
+                        <input type="checkbox" name="listed" id="listed">
+                    </span>
+                    <button type="submit" class="bg-blue-600">Submit</button>
+                </form>
+            </div>
+            
         </div>
     </header-layout>
 </template>
@@ -11,6 +26,17 @@
     export default {
         components: {
             HeaderLayout
-        }
+        },
+        methods: {
+            submit() {
+                var data = new FormData()
+                data.append('title', title.value || '')
+                data.append('description', description.value || '')
+                data.append('video', video.files[0] || '')
+                data.append('listed', listed.value || '')
+
+                this.$inertia.post('/upload', data)
+            }
+        },
     }
 </script>
