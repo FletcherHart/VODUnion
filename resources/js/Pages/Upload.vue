46ce4638073ef7acce846662044877017a13f94c
FletcherHart <fletcherhart@gmail.com>
2020-12-10 15:19:21 -0500

    Add error descriptions below related inputs

--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -7,12 +7,16 @@
             <div class="flex justify-center">
                 <form @submit.prevent="submit" class="grid grid-cols-1 w-1/3">
                     <input class="border-solid border-2 border-black-600" id="title" name="title" placeholder="Video Title" >
+                    <div v-if="errors.title"><mark>{{ errors.title }}</mark></div>
                     <input class="border-solid border-2 border-black-600" id="description" name="description" rows=5 placeholder="Description">
+                    <div v-if="errors.description"><mark>{{ errors.description }}</mark></div>
                     <input type="file" id="video" name="video" ref="video" accept=".mp4">
+                    <div v-if="errors.video"><mark>{{ errors.video }}</mark></div>
                     <span>
                         <label for="listed">List video immediatly upon upload?</label>
                         <input type="checkbox" name="listed" id="listed">
                     </span>
+                    <div v-if="errors.listed"><mark>{{ errors.listed }}</mark></div>
                     <button type="submit" class="bg-blue-600">Submit</button>
                 </form>
             </div>
@@ -26,6 +30,9 @@
     export default {
         components: {
             HeaderLayout
+        },
+        props: {
+            errors: Object
         },
         methods: {
             submit() {
