276b0efe62b693fd1e04087a9521e4123220eb37
FletcherHart <fletcherhart@gmail.com>
2020-12-18 16:31:49 -0500

    Add front end portion for uploading thumbnails and raw video
    Form expanded to include options for raw video + thumbnail upload to coincide with previous commit

--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -9,18 +9,36 @@
                     <mark>{{errors.storage}}</mark>
                 </div>
                 <div class="h-full w-full flex justify-center">
-                    <form @submit.prevent="submit" class="grid grid-cols-1 w-1/3">
+                    <form @submit.prevent="submit" class="grid grid-cols-1 xl:w-1/3">
                         <input class="border-solid border-2 border-black-600" id="title" name="title" placeholder="Video Title" >
                         <div v-if="errors.title"><mark>{{ errors.title }}</mark></div>
+
                         <input class="border-solid border-2 border-black-600" id="description" name="description" rows=5 placeholder="Description">
                         <div v-if="errors.description"><mark>{{ errors.description }}</mark></div>
-                        <input type="file" id="video" name="video" ref="video" accept=".mp4">
+
+                        <span>
+                            <label for="video">Video: </label>
+                            <input type="file" id="video" name="video" ref="video" accept=".mp4">
+                        </span>
                         <div v-if="errors.video"><mark>{{ errors.video }}</mark></div>
+
+                        <span>
+                            <label for="thumb">Thumbnail: </label>
+                            <input type="file" id="thumb" name="thumb" ref="thumb" accept=".jpeg, .jpg, .png">
+                        </span>
+                        <div v-if="errors.thumb"><mark>{{ errors.thumb }}</mark></div>
+
                         <span>
                             <label for="listed">List video immediatly upon upload?</label>
                             <input type="checkbox" name="listed" id="listed">
                         </span>
                         <div v-if="errors.listed"><mark>{{ errors.listed }}</mark></div>
+
+                        <span>
+                            <label for="raw">Maintain raw video quality?</label>
+                            <input type="checkbox" name="raw" id="raw">
+                        </span>
+
                         <button type="submit" class="bg-blue-600">Submit</button>
                     </form>
                 </div>
@@ -45,7 +63,9 @@
                 data.append('title', title.value || '')
                 data.append('description', description.value || '')
                 data.append('video', video.files[0] || '')
+                data.append('thumbnail', thumb.files[0] || '')
                 data.append('listed', listed.value || '')
+                data.append('raw', raw.value || '')
 
                 this.$inertia.post('/upload', data)
             }
