24d495ec5a49b03f9618cf4029b0e33805a97e4e
FletcherHart <fletcherhart@gmail.com>
2020-12-28 12:10:18 -0500

    Change video source to cloudflare provided url, previous div container commented out

--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -2,9 +2,10 @@
     <header-layout>
         <div class="h-auto -mt-4">
             <div class="h-screen">
-                <div class="bg-black w-full flex items-center justify-center h-5/6">
-                    <video class="h-full" :src="'/stream/' + data.id" controls> </video>
-                </div>
+                <!-- <div class="bg-black w-full flex items-center justify-center h-5/6">
+                    <iframe class="h-full" :src="'https://watch.cloudflarestream.com/' + data.videoID" controls> </iframe>
+                </div> -->
+                <iframe class="h-full w-full" :src="'https://iframe.videodelivery.net/'+ data.videoID +'?autoplay=true'" allow="accelerometer; gyroscope; autoplay; encrypted-media;" allowfullscreen="true"></iframe>
             </div>
             <div class="mt-3 flex justify-center">
                 <div class="w-11/12">
