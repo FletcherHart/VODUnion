646103c206f72495f2fb5673d2c7412e08d822c5
FletcherHart <fletcherhart@gmail.com>
2020-12-28 12:31:10 -0500

    Create Error.vue + Add header & display preliminary error

---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,19 @@
+<template>
+    <header-layout>
+        <div>
+            {{errors.deny}}
+        </div>
+    </header-layout>
+</template>
+
+<script>
+    import HeaderLayout from '@/Layouts/HeaderLayout'
+    export default {
+        components: {
+            HeaderLayout
+        },
+        props: {
+            errors: Object,
+        },
+    }
+</script>
