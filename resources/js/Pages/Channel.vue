6b7b478cb52f712ed823ab9c918331109fa2a1dd
FletcherHart <fletcherhart@gmail.com>
2020-12-29 16:22:02 -0500

    Change Channel.vue to handle multiple forms, display errors in temp modal, remove unused inputs

--- resources/js/Pages/Channel.vue
+++ resources/js/Pages/Channel.vue
@@ -1,28 +1,25 @@
 <template>
     <header-layout>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-full">
-            <div class="overflow-hidden grid grid-cols-1 gap-4">
-                <div class="w-full flex flex-row justify-between border-2 border-black" v-for="video in data" :key="video.id">
-                    <div class="w-40">
-                        <inertia-link class="flex flex-col" :href="'video/'+video.id">
-                            <div class="w-full bg-black flex justify-center ">
-                                <img :src="'https://videodelivery.net/' +video.videoID+'/thumbnails/thumbnail.jpg?time=0s&height=270'">
-                            </div>
-                            {{video.title}} 
-                        </inertia-link>
+            <div class="overflow-hidden flex flex-col justify-center">
+                <div class="w-full grid grid-cols-3 gap-4 border-2 border-black" v-for="video in data" :key="video.id">
+                    <div class="w-40 sm:w-auto">
+                        <div class="w-full bg-black flex justify-center ">
+                            <img :src="'https://videodelivery.net/' +video.videoID+'/thumbnails/thumbnail.jpg?time=0s&height=270'">
+                        </div>
                     </div>
-                    <form @submit.prevent="submit" class="grid grid-cols-1">
-                        <input class="border-solid border-2 border-black-600" id="title" name="title" placeholder="Video Title" >
+                    <form @submit.prevent="submit(video.id)" class="grid grid-cols-1 col-span-2" :id="video.id">
+                        <span>
+                            <label for="title">Title: </label>
+                            <input class="border-solid border-2 border-black-600" id="title" name="title" placeholder="Video Title" :value="video.title">
+                        </span>
                         <div v-if="errors.title"><mark>{{ errors.title }}</mark></div>
 
-                        <input class="border-solid border-2 border-black-600" id="description" name="description" rows=5 placeholder="Description">
-                        <div v-if="errors.description"><mark>{{ errors.description }}</mark></div>
-
-                        <span>
-                            <label for="video">Video: </label>
-                            <input type="file" id="video" name="video" ref="video" accept=".mp4">
+                        <span class="w-full">
+                            <label for="description">Description:</label>
+                            <textarea class="border-solid border-2 border-black-600 resize-none" id="description" name="description" rows=5 placeholder="Description" :value="video.description"/>
                         </span>
-                        <div v-if="errors.video"><mark>{{ errors.video }}</mark></div>
+                        <div v-if="errors.description"><mark>{{ errors.description }}</mark></div>
 
                         <span>
                             <label for="thumb">Thumbnail: </label>
@@ -31,21 +28,24 @@
                         <div v-if="errors.thumb"><mark>{{ errors.thumb }}</mark></div>
 
                         <span>
-                            <label for="listed">List video immediatly upon upload?</label>
-                            <input type="checkbox" name="listed" id="listed">
+                            <label for="list">List video immediatly upon upload?</label>
+                            <input v-if="video.list == 0" type="checkbox" name="list" id="list">
+                            <input v-else type="checkbox" name="list" id="list" checked>
                         </span>
-                        <div v-if="errors.listed"><mark>{{ errors.listed }}</mark></div>
-
-                        <span>
-                            <label for="raw">Maintain raw video quality?</label>
-                            <input type="checkbox" name="raw" id="raw">
-                        </span>
+                        <div v-if="errors.list"><mark>{{ errors.list }}</mark></div>
 
                         <button type="submit" class="bg-blue-600">Submit</button>
                     </form>
                 </div>
             </div>
         </div>
+        <div v-if="Object.keys(this.errors).length > 0" class="fixed top-0 left-0 flex items-center justify-center w-full h-full">
+            <div v-bind:class="{'lg:ml-52 md:ml-40 sm:ml-24': !this.$store.state.isHidden}"
+            class="lg:mb-52 md:mb-40 sm:mb-24 bg-white opacity-100 w-1/3 h-1/3">
+                <mark>{{Object.values(errors)[0]}}</mark>
+            </div>
+        </div>
+        <div v-if="Object.keys(this.errors).length > 0" class="bg-black opacity-25 w-full h-full"></div>
     </header-layout>
 </template>
 
@@ -53,11 +53,18 @@
     import HeaderLayout from '@/Layouts/HeaderLayout'
     export default {
         components: {
-            HeaderLayout
+            HeaderLayout,
         },
         props: {
             data: Array,
             errors: Object
-        }
+        },
+        methods: {
+            submit(id) {
+                console.log(Object.keys(this.errors).length)
+                var data = new FormData(document.getElementById(id))
+                this.$inertia.post('/channel/'+id, data)
+            }
+        },
     }
 </script>
