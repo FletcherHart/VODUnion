777145086aac17a6e241253e0cb5782179d962e7
FletcherHart <fletcherhart@gmail.com>
2021-01-04 13:25:58 -0500

    Merge Upload & Channel pages into single page, change conflicting variables, add Inertia reload + form clearing on submit

--- resources/js/Pages/Channel.vue
+++ resources/js/Pages/Channel.vue
@@ -2,6 +2,29 @@
     <header-layout>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-full">
             <div class="overflow-hidden flex flex-col justify-center">
+                <div> 
+                    <button v-on:click="uploadModalDisplay">Upload</button>
+                    <div v-bind:class="{'hidden': uploadModal}">
+                        <div class="flex justify-center">
+                            <h2 class="m-auto font-bold">Upload</h2>
+                        </div>
+                        <div v-if="progress > 0" class="w-full flex justify-center">
+                            Progress: {{progress.toFixed(1)}}%
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="h-full w-full flex justify-center">
+                                <form @submit.prevent="upload" class="grid grid-cols-1 xl:w-1/3">
+                                    <span>
+                                        <label for="video">Video: </label>
+                                        <input type="file" id="video" name="video" ref="video" accept=".mp4">
+                                    </span>
+                                    <div v-if="uploadErrors.length > 0"><mark>{{ uploadErrors }}</mark></div>
+                                    <button type="submit" v-bind:class="{'bg-blue-600':!progress, 'bg-gray-400': progress}" :disabled="progress > 0">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="w-full grid grid-cols-3 gap-4 border-2 border-black" v-for="video in data" :key="video.id">
                     <div class="w-40 sm:w-auto">
                         <div class="w-full bg-black flex justify-center ">
@@ -57,11 +80,13 @@
             </div>
             <div class="bg-black opacity-75 w-full h-full absolute top-0 left-0"></div>
         </div>
+
     </header-layout>
 </template>
 
 <script>
     import HeaderLayout from '@/Layouts/HeaderLayout'
+    import { Inertia } from '@inertiajs/inertia'
     export default {
         components: {
             HeaderLayout,
@@ -69,6 +94,15 @@
         props: {
             data: Array,
             errors: Object
+        },
+        data() {
+            return {
+                uploadModal: Boolean,
+                url: "",
+                progress: 0,
+                uploadErrors: "",
+                videoList: 0,
+            }
         },
         methods: {
             submit(id) {
@@ -83,6 +117,72 @@
             },
             deleteVideo(id) {
                 this.$inertia.delete('/channel/'+id);
+            },
+            uploadModalDisplay() {
+                this.uploadModal = !this.uploadModal
+            },
+            upload() {
+                this.uploadErrors = "";
+
+                var vid = new FormData()
+                vid.append('file', video.files[0] || '')
+                const config = {
+                    onUploadProgress: progressEvent => this.displayProgress(progressEvent.loaded/progressEvent.total)
+                }
+
+                if(this.url.length == 0) {
+                    this.fetchKey().then( data => {
+                        console.log(data[1]);
+                        if(data == null) {
+                            this.uploadErrors = "There was an error processing your request. Please try again later.";
+                        } else {
+                            this.url = data[1];
+
+                            axios.post(this.url, vid, config).then(response => {
+                               this.progress = 0;
+                               this.uploadModalDisplay();
+                               this.$refs.video.value = null;
+                               Inertia.reload();
+                            }).catch(error => {
+                                if(error.response) {
+                                    this.progress = 0;
+                                    console.log(error.response.data);
+                                    console.log(error.response.status);
+                                    if(error.response.status == 400) {
+                                        this.uploadErrors = "Error: The chosen file is not supported. Please use an mp4 file."
+                                    }
+                                }
+                            });
+
+                        }
+                        
+                    })
+                } else {
+                    axios.post(this.url, vid, config).then(x => {
+                        this.progress = 0;
+                        this.uploadModalDisplay();
+                        this.$refs.video.value = null;
+                        Inertia.reload();
+                    }).catch(error => {
+                            if(error.response) {
+                                this.uploadErrors = error.message;
+                            }
+                        });
+                }
+            },
+            fetchKey() {
+                return fetch('key')
+                    .then(response =>  {
+                        if(response.status == 200)  {
+                            return response.json();
+                        }
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+            },
+            displayProgress(percent) {
+                this.progress = percent*100;
             }
         },
     }
