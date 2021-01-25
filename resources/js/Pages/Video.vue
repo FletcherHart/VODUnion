3a43d3700ea727794df25331a1a6537bb833d24d
FletcherHart <fletcherhart@gmail.com>
2021-01-25 11:42:38 -0500

    Increase display size of videos

--- resources/js/Pages/Video.vue
+++ resources/js/Pages/Video.vue
@@ -2,12 +2,8 @@
     <header-layout>
         <div class="h-auto -mt-4">
             <div class="h-screen">
-                <!-- <div class="bg-black w-full flex items-center justify-center h-5/6">
-                    <iframe class="h-full" :src="'https://watch.cloudflarestream.com/' + data.videoID" controls> </iframe>
-                </div> -->
-                <iframe class="h-full w-full" :src="'https://iframe.videodelivery.net/'+ data.videoID +'?autoplay=true'" allow="accelerometer; gyroscope; autoplay; encrypted-media;" allowfullscreen="true"></iframe>
-            </div>
-            <div class="mt-3 flex justify-center">
+                <iframe class="h-3/4 w-full" :src="'https://iframe.videodelivery.net/'+ data.videoID +'?autoplay=true'" allow="accelerometer; gyroscope; autoplay; encrypted-media;" allowfullscreen="true"></iframe>
+                <div class="mt-3 flex justify-center">
                 <div class="w-11/12">
                     <div class="flex items-center justify-between">
                         <h2>{{data.title}}</h2>
@@ -16,13 +12,14 @@
                     <hr>
                     <div>
                         <div class="cursor-pointer" @click="toggleDesc">Show All</div>
-                        <div v-bind:class="{'truncate': hideDescription, 'h-auto min-h-full': !hideDescription}">{{data.description}}</div>
+                        <div class="break-words" v-bind:class="{'truncate': hideDescription, 'h-auto min-h-full': !hideDescription}">{{data.description}}</div>
                     </div>
                 </div>
             </div>
+            </div>
         </div>
         
-        <comment-layout></comment-layout>
+        <!-- <comment-layout></comment-layout> -->
     </header-layout>
 </template>
 
