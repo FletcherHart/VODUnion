3fd5cec6102b5694606241e8036d96d5792cd507
FletcherHart <fletcherhart@gmail.com>
2021-02-12 12:57:08 -0500

    Display total comments on video page, show errors, clear input on successful post

--- resources/js/Layouts/CommentLayout.vue
+++ resources/js/Layouts/CommentLayout.vue
@@ -5,14 +5,19 @@
               <form @submit.prevent="submit">
                 <input class="border-solid border-2 border-black-600" id="text" type="textarea" v-model="form.text" placeholder="Comment" />
                 <button type="submit">Submit</button>
+                <div>
+                <jet-input-error v-if="form.errors.text" :message="form.errors.text"/>
+                </div>
               </form>
             </div>
             <div v-else> 
               Please login to post a comment.
             </div>
           <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+              <h1 class="text-xl font-bold">Total Comments: {{totalComments}}</h1>
+              <hr>
               <div v-for="comment in $page.props.comments" :key="comment.id">
-                <div class="my-3">
+                <div class="my-3 ml-2 mr-2">
                   <!-- <a class="inline text-blue-600 hover:text-purple-600 mb-3" :href="'user/' + comment.user_id">{{comment.name}}</a> -->
                   <div class="flex justify-between">
                     <div>
@@ -25,7 +30,7 @@
                       v-if="$page.props.user.id == comment.user_id || $page.props.user.role_id == 4 || $page.props.user.id == ownerID">Delete</button>
                      </div>
                   </div>
-                  <p>{{comment.text}}</p>
+                  <p class="break-words">{{comment.text}}</p>
                 </div>
                 <hr>
               </div>
@@ -35,22 +40,40 @@
 </template>
 
 <script>
+import JetInputError from '../Jetstream/InputError'
 export default {
+  components: {
+    JetInputError,
+  },
   props: {
     id: Number,
-    ownerID: Number
+    ownerID: Number,
+    totalComments: Number,
+    errors: Object
   },
   data() {
     return {
-      form: {
+      form: this.$inertia.form({
+        '_method': 'POST',
         text:null
-      },
-    }
+      }, {
+              bag: 'comment',
+              resetOnSuccess: false,
+          }),
+        }
+  },
+  updated: function() {
+      if(this.form.recentlySuccessful) {
+          this.form.reset();
+          this.form.recentlySuccessful = false;
+      }
   },
   methods: {
     submit() {
-      this.$inertia.post('/video/' + this.id + "/comment", this.form, {preserveScroll:true});
-      this.form.text = null;
+      this.form.post('/video/' + this.id + '/comment', {
+          preserveScroll: true
+      });
+      //this.form.text = null;
     },
     deleteComment(id) {
       this.$inertia.delete('/comment/'+id, {preserveScroll:true});
