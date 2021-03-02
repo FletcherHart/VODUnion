0669953d9eeaa141194e7a3329c61b7c896ba605
FletcherHart <fletcherhart@gmail.com>
2021-03-02 13:21:59 -0500

    Add explanation on upgrade page

--- resources/js/Pages/Upgrade.vue
+++ resources/js/Pages/Upgrade.vue
@@ -1,8 +1,11 @@
 <template>
     <header-layout>
-        <div class="flex items-center justify-center">
-            <h1>Upgrade</h1>
-            <form @submit.prevent="submit">
+        <div class="flex items-center justify-center flex-col">
+            <div class="font-bold">Upgrade</div>
+            <div class="w-1/3 break-words">Video uploads are restricted to verified users at this time. Users with a code may enter it below, which will upgrade your account and
+                allow you to upload up to <b>3</b> videos.
+            </div>
+            <form @submit.prevent="submit" class="mt-5">
                 <span>
                     <label for="code">Code:</label>
                     <input name="code" id="code" v-model="form.code">
