25f1e5632769db92495b838bbc6986161f9b2590
FletcherHart <fletcherhart@gmail.com>
2021-03-05 13:00:17 -0500

    Create LikeVideo model for liking videos

---
+++ app/Models/LikeVideo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LikeVideo extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['text', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function video()
+    {
+        return $this->belongsTo('App\Models\Video');
+    }
+}
