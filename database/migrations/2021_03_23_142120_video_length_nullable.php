9b44b294249ec2d9d54fd76bbccb8f408e25c5c2
FletcherHart <fletcherhart@gmail.com>
2021-03-23 11:10:49 -0400

    Update migration to make video_length default to zero

---
+++ database/migrations/2021_03_23_142120_video_length_nullable.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class VideoLengthNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('videos', function (Blueprint $table) {
+            $table->decimal('video_length')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
