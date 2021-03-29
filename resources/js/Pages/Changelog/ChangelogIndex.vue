25293c680789b849a1f3c4d3a393770c673c0712
FletcherHart <fletcherhart@gmail.com>
2021-03-29 11:28:46 -0400

    Add styling to past changelog display and change date format

--- resources/js/Pages/Changelog/ChangelogIndex.vue
+++ resources/js/Pages/Changelog/ChangelogIndex.vue
@@ -1,16 +1,21 @@
 <template>
     <header-layout>
-        <div class="sm:flex flex-col sm:items-center mb-10">
+        <div class="sm:flex flex-col sm:items-center m-10 mt-0">
             <div class="sm:w-4/6 flex items-center justify-center flex-col whitespace-pre-wrap">
                 <h1 class="font-bold text-2xl">{{changelog.title}}</h1>
                 <div>{{changelog.text}}</div>
             </div> 
-            <div class="sm:w-4/6 w-full h-40 bg-white rounded">
+            <div class="sm:w-4/6 w-full h-40 bg-white rounded mt-5">
                 <div v-for="log in past_logs" :key="log.id">
+                    <div v-bind:class="{'bg-yellow-200': (log.id == changelog.id)}">
                     <inertia-link :href="'changelog/'+log.id">
+                        <div class="relative">
+                            <img v-if="(log.id == changelog.id)" class="absolute right-0" width="16px" height="16px" src="/open-iconic/svg/star.svg" alt="current changelog article icon">
+                        </div>
                         <div>{{log.title}}</div>
                         <div class="text-gray-600">{{toDate(log.created_at)}}</div>
                     </inertia-link>
+                    </div>
                     <hr>
                 </div>
             </div>
@@ -22,7 +27,7 @@
 <script>
     import HeaderLayout from '@/Layouts/HeaderLayout'
     import JetInputError from '@/Jetstream/InputError'
-    import {toDate} from 'date-fns'
+    import {toDate, format} from 'date-fns'
     export default {
         components: {
             HeaderLayout,
@@ -51,7 +56,7 @@
                 });
             },
             toDate(date) {
-                return toDate(new Date(date));
+                return format(toDate(new Date(date)), 'LLLL io, y');
             },
         }
     }
