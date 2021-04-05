d01e0c0912275715700c57bf91de006454393022
FletcherHart <fletcherhart@gmail.com>
2021-04-05 12:22:14 -0400

    Change header layout styling & color Change Home link to display VODUnion with colored background Change search bar to sharp corners instead of rounded Remove hover effect for email, underline email Change ResponsiveNavLink to dark background & white text on hover

--- resources/js/Layouts/HeaderLayout.vue
+++ resources/js/Layouts/HeaderLayout.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="h-full"> 
-    <header class="shadow-md  bg-gray-700 fixed top-0 w-full h-24 sm:h-16 z-30">
+    <header class="fixed top-0 w-full h-24 sm:h-16 z-30 nav-header">
       <div class="flex flex-col h-full">
         <div class="flex justify-between sm:h-full h-16">
           <div class="flex flex-row items-center justify-center ml-6">
-            <button @click="nav" class="hover:bg-white p-2 rounded focus:outline-none w-10 flex justify-center" aria-label="display menu">
+            <button @click="nav" class="hover:bg-gray-600 p-2 rounded focus:outline-none w-10 flex justify-center" aria-label="display menu">
               <img width="16px" height="16px" src="/open-iconic/svg/menu.svg" alt="menu icon" aria-hidden="true">
             </button>
-            <jet-responsive-nav-link href="/">Home</jet-responsive-nav-link>
+            <a href="/" class="block pl-3 pr-4 py-2 ml-5 font-semibold rounded hover:rounded text-black" style="background-color:#7DAEFC;">VODUnion</a>
           </div>
           
           <form class="flex items-center justify-center" @submit.prevent="search" v-if="this.windowWidth > 640">
-            <input class="rounded rounded-r-none w-96 h-8" type="text" id="search" aria-label="Search for videos">
+            <input class="w-96 h-8 border-black border p-1" type="text" id="search" placeholder="Search" aria-label="Search for videos">
             <button class="rounded-r icon-button h-8 w-8 flex items-center justify-center bg-gray-600" type="submit" aria-label="submit">
               <img width="16px" height="16px" src="/open-iconic/svg/magnifying-glass.svg" alt="search icon" aria-hidden="true">
             </button>
           </form>
 
-          <div class="flex items-center text-white font-medium mr-6">
+          <div class="flex items-center font-medium mr-6">
               <div class="text-center">Have questions? Email 
-                <a class="hover:bg-white hover:text-black underline text-color" href="mailto:support@vodunion.com">support@vodunion.com</a>
+                <a class="underline" href="mailto:support@vodunion.com">support@vodunion.com</a>
               </div>
           </div>
         </div>
         <form class="shadow-md flex items-center justify-center fixed top-16 w-full m-b-8" @submit.prevent="search" v-if="this.windowWidth < 640">
-          <input class="rounded rounded-r-none w-full h-8" type="text" id="search" aria-label="Search for videos">
+          <input class="rounded rounded-r-none w-full h-8 p-1" type="text" id="search" placeholder="Search" aria-label="Search for videos">
           <button class="rounded-r icon-button h-8 w-8 flex items-center justify-center bg-gray-600" aria-label="submit">
             <img width="16px" height="16px" src="/open-iconic/svg/magnifying-glass.svg" alt="search icon" aria-hidden="true">
           </button>
@@ -35,7 +35,7 @@
     <nav v-bind:class="{ hidden: this.$store.state.isHidden}">
       <sidebar/>
     </nav>
-    <main class="overflow-auto" v-bind:class="{ 'pt-20': (this.windowWidth > 640), 'pt-28': (this,windowWidth < 640)}">
+    <main class="overflow-auto border-t border-black relative top-16" v-bind:class="{'pt-10': (this,windowWidth < 640)}">
       <div class="h-auto" v-bind:class="{ 
         'w-full': this.$store.state.isHidden, 
         'lg:ml-52 md:ml-40 sm:ml-24': !this.$store.state.isHidden
