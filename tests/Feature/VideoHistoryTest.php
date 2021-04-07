d30a66ce534e818a382cca963750fd0d542e8877
FletcherHart <fletcherhart@gmail.com>
2021-04-07 12:42:34 -0400

    Create basic video history auth test

---
+++ tests/Feature/VideoHistoryTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Http\UploadedFile;
+use App\Models\User;
+use App\Models\Video;
+use App\Models\LikeVideo;
+
+class VideoHistoryTest extends TestCase
+{
+
+    use RefreshDatabase, WithFaker, DatabaseMigrations;
+    
+    public function setUp():void {
+        parent::setUp();
+        $this->seed();
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_authenticated_user_can_see_history_page()
+    {
+        $this->be($user = User::factory()->create());
+        $response = $this->get('/history');
+
+        dump($response);
+
+        $response->assertStatus(200);
+    }
+
+    public function test_non_auth_user_redirected_to_login()
+    {
+        $response = $this->get('/history');
+
+        $response->assertStatus(302);
+    }
+}
