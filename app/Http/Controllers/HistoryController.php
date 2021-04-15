e4574e9bc5d2f36c0a57bd8e6895c2d3fff09543
FletcherHart <fletcherhart@gmail.com>
2021-04-15 12:45:00 -0400

    Change history controller to use eloquent models

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Video;
+use App\Models\WatchedVideo;
 use App\Models\Comment;
 use Illuminate\Support\Facades\DB;
 
@@ -13,14 +14,13 @@ class HistoryController extends Controller
     public function watchedVideos(Request $request) {
 
         $videos_to_get = 12;
-        $max_videos = DB::table('watched_videos')->where('watched_videos.user_id', '=', auth()->id())->count();
+        $max_videos = WatchedVideo::where('watched_videos.user_id', '=', auth()->id())->count();
         
         if($request['num_videos'] < $max_videos && $request['num_videos'] != null) {
             $videos_to_get += $request['num_videos'];
         }
 
-        $videos = DB::table('videos')
-            ->join('watched_videos', 'videos.id', '=', 'watched_videos.video_id')
+        $videos = Video::join('watched_videos', 'videos.id', '=', 'watched_videos.video_id')
             ->join('users', 'videos.user_id', '=', 'users.id')
             ->where('watched_videos.user_id', '=', auth()->id())
             ->select('videos.*', 'users.name as uploader')
