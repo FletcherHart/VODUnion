411defc3b453a42a3251766ff572f5decfa6ef1e
FletcherHart <fletcherhart@gmail.com>
2021-04-15 12:17:03 -0400

    Reformat comment history layout

--- resources/js/Components/CommentHistory.vue
+++ resources/js/Components/CommentHistory.vue
@@ -3,20 +3,15 @@
         <article class="shadow border border-black m-2" v-for="comment in comments" :key="comment.id">
             <div class="my-3 ml-2 mr-2">
                   <!-- <a class="inline text-blue-600 hover:text-purple-600 mb-3" :href="'user/' + comment.user_id">{{comment.name}}</a> -->
-                  <div class="flex justify-between">
-                    <div>
-                      <p class="inline">{{comment.name}}</p>
-                      <p class="inline">Posted {{getAge(comment.created_at)}}</p>
+                    <p class="break-words whitespace-pre-line whitespace-pre-wrap">{{comment.text}}</p>
+                    <div class="flex justify-between mt-5">
+                        <div class="self-end">
+                            Commented {{getAge(comment.created_at)}} on <a class="underline text-blue-600" :href="route('video', comment.video_id)">{{comment.title}}</a>
+                        </div>
+                        <button class="bg-red-600 text-white rounded ml-2 mr-2 p-2"
+                        v-on:click="deleteComment(comment.id)">Delete</button>
                     </div>
-                    <div v-if="$page.props.auth.user.loggedIn">
-                      <button class="bg-red-600 text-white rounded ml-2 mr-2 p-2"
-                      v-on:click="deleteComment(comment.id)">Delete</button>
-                     </div>
-                  </div>
-                  <p class="break-words whitespace-pre-line whitespace-pre-wrap">{{comment.text}}</p>
-                  <div>
-                      Commented on <a class="underline text-blue-600" :href="route('video', comment.video_id)">{{comment.title}}</a>
-                  </div>
+                  
                 </div>
                 <hr>
         </article>
