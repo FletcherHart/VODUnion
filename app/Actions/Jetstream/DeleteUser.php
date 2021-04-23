346f088776bd69a5ef280c9f61dbc948dc510f24
FletcherHart <fletcherhart@gmail.com>
2021-04-23 10:41:36 -0400

    Fix issue with videos not being removed on account deletion

--- app/Actions/Jetstream/DeleteUser.php
+++ app/Actions/Jetstream/DeleteUser.php
@@ -3,6 +3,9 @@
 namespace App\Actions\Jetstream;
 
 use Laravel\Jetstream\Contracts\DeletesUsers;
+use App\Http\VideoController;
+use App\Models\Video;
+use Illuminate\Support\Facades\Http;
 
 class DeleteUser implements DeletesUsers
 {
@@ -14,6 +17,17 @@ class DeleteUser implements DeletesUsers
      */
     public function delete($user)
     {
+        $videos = Video::where('user_id', $user->id)->get();
+
+        foreach($videos as $video) {
+            Http::withToken(config('app.cloud_token'))
+            ->delete('https://api.cloudflare.com/client/v4/accounts/'
+            . config('app.cloud_account') .
+            '/stream/' . $video->videoID);
+
+            $video->delete();
+        }
+
         $user->deleteProfilePhoto();
         $user->tokens->each->delete();
         $user->delete();
