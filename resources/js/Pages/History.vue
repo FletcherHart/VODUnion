bbde72387006205ed44a071efbd0aa3b034a1c1c
FletcherHart <fletcherhart@gmail.com>
2021-04-27 10:23:34 -0400

    Change history page links to inertia-links

--- resources/js/Pages/History.vue
+++ resources/js/Pages/History.vue
@@ -4,8 +4,8 @@
             <h1 v-if="comments != null" class="font-bold text-xl">Comment History</h1>
             <h1 v-if="videos != null" class="font-bold text-xl">Watched Videos History</h1>
             <div class="flex justify-center mt-5">
-                <a :href="route('history')" class="bg-blue-600 text-white rounded p-2 m-1">Video History</a>
-                <a :href="route('comments')" class="bg-blue-600 text-white rounded p-2 m-1">Comment History</a>
+                <inertia-link :href="route('history')" class="bg-blue-600 text-white rounded p-2 m-1">Video History</inertia-link>
+                <inertia-link :href="route('comments')" class="bg-blue-600 text-white rounded p-2 m-1">Comment History</inertia-link>
             </div>
             <comment-history v-if="comments != null" :comments="comments"></comment-history>
             <video-history-component v-if="videos != null" :videos="videos" :maxVideos="maxVideos"></video-history-component>
